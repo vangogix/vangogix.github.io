3d4dbcfb84166d9928d9906386974974fd80b97d
Игорь <38939018+vangogix@users.noreply.github.com>
2018-05-03 23:57:32 +0400

    New menu

---
+++ js/index.js
@@ -0,0 +1,30 @@
+$(function(){
+  /*--------- show and hide the menu  ---*/
+  $('.button').on("click", function(){
+    if($('body').hasClass('nav_is_visible') == true){
+     $('body').removeClass('nav_is_visible');
+     $('.button').removeClass('close');
+        }
+    else{
+     $('body').addClass('nav_is_visible');
+     $('.button').addClass('close');
+       }
+   });
+  
+  $('body').addClass('home_is_visible');
+
+    
+ function removeClasses() {
+  $(".menu ul li").each(function() {
+    var custom_class = $(this).find('a').data('class');
+  $('body').removeClass(custom_class);
+  });
+}
+  
+  $('.menu a').on('click',function(e){
+    e.preventDefault();
+    removeClasses();
+    var custom_class = $(this).data('class');
+    $('body').addClass(custom_class);
+  });
+});
